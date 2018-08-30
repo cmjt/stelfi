57bb94137dc7d787eb1450f7873222b8e188d7a9
cmjt <cmjt@st-andrews.ac.uk>
2018-08-31 06:31:28 +1200

    serial killer data

---
+++ R/data.r
@@ -0,0 +1,17 @@
+#' A dataset of recorded serial killers in the UK
+#' constructed from http://www.murderuk.com/
+#' @name serialUK
+#' @format A dataframe with 62 rows and 8 variables
+#' \describe{
+#' \item {Number.of.kills}{approximate number of murders commited by each person}
+#' \item {Years}{The years of "operation"}
+#' \item {Name}{Name of convicted serial kiler}
+#' \item {AKA}{Some serial killers were given nicknames}
+#' \item {Year.Start}{Year the murders began}
+#' \item {Year.End}{Year the murders ended}
+#' \item {Date.of,first.kill}{Date, if known, first murder was commited}
+#' \item {population..M.}{Estimated population of UK in millions at time of first murder}
+#' }
+#' @docType data
+#' @usage data(serialUK)
+NULL
