383cdbf02a2aed1cc339bf44c30a255ed8d7f071
cmjt <cmjonestodd@gmail.com>
2019-08-07 15:04:55 +1200

    still trying to access sub vector

--- inst/src/hawkes.cpp
+++ inst/src/hawkes.cpp
@@ -1,4 +1,6 @@
 #include <TMB.hpp>
+#include <vector>
+#include <iostream>
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
@@ -9,23 +11,24 @@ Type objective_function<Type>::operator() ()
   PARAMETER(mu);
   PARAMETER(alpha);
   PARAMETER(beta);
-  std::cout << beta << "\n";
-  Type term_1 = -mu*times(times.size());
+  Type last = times[times.size() - 1];
+  std::cout << last << "\n";
+  Type term_1 = -mu*last;
   std::cout << term_1 << "\n";
-  vector<Type> term_2vec = alpha/beta*(exp(-beta * (times(times.size()) - times)) - 1);
+  vector<Type> term_2vec = alpha/beta*(exp(-beta * (last - times)) - 1);
   std::cout << term_2vec << "\n";
   Type term_2 = sum(term_2vec);
   vector<Type> A;
   Type nll = 0;
   for(int i = 1; i <times.size(); i++){
-    vector<Type> sub(0,(i - 1));
+    Type sub = times[0];
     std::cout << sub << "\n";
-    vector<Type> temp = -beta*sub;
-    A(i) += sum(exp(temp));
+    // vector<Type> temp = -beta*sub;
+    // A(i) += sum(exp(temp));
   }
-  vector<Type> mualphaA = mu + alpha*A;
-  Type term_3 = sum(log(mualphaA));
-  nll = -term_1 - term_2 - term_3;
+  // vector<Type> mualphaA = mu + alpha*A;
+  // Type term_3 = sum(log(mualphaA));
+  // nll = -term_1 - term_2 - term_3;
   std::cout << nll << "\n";
   return nll;
 }
