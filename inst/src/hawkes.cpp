13a9f425f66a4ff46abd845648ae2743a18a78c5
cmjt <cmjt@st-andrews.ac.uk>
2019-09-03 13:24:50 +1200

    ll hawkes working

--- inst/src/hawkes.cpp
+++ inst/src/hawkes.cpp
@@ -11,21 +11,21 @@ Type objective_function<Type>::operator() ()
   PARAMETER(mu);
   PARAMETER(alpha);
   PARAMETER(beta);
+  // t_n
   Type last = times[times.size() - 1];
   Type term_1 = -mu*last;
-  vector<Type> term_2vec = alpha/beta*(exp(-beta * (last - times)) - 1);
-  Type term_2 = sum(term_2vec);
-  vector<Type> A;
-  Type nll = 0;
-  for(int i = 1; i <=times.size(); i++){
-    vector<Type> sub = times.head(i-1);
+  vector<Type> term_2vec = exp(-beta * (last - times)) - 1;
+  Type term_2 = alpha/beta*sum(term_2vec);
+  vector<Type> A(times.size());
+  Type ll = 0;
+  for(int i = 1; i <= times.size(); i++){
+    vector<Type> sub = times(i - 1) - times.head(i - 1);
     vector<Type> temp = -beta*sub;
-    Type etemp = sum(exp(temp));
-    Type mualphaA = mu + alpha*etemp;
-    Type term_3 = log(mualphaA);
-    nll += -term_1 - term_2 - term_3;
+    A(i - 1) = sum(exp(temp));
   }
-  std::cout << nll << "\n";
-  return nll;
+  vector<Type> As = log(mu + alpha*A);
+  Type term_3 =  sum(As);
+  ll = -term_1 - term_2 - term_3;
+  return ll;
 }
 
