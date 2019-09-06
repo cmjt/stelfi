9163d1186ce9cac19beda8bd20bb0bcf6b34162b
cmjt <cmjonestodd@gmail.com>
2019-09-06 12:24:02 +1200

    cleaning NIWA retweets data

--- R/data.r
+++ R/data.r
@@ -4,12 +4,12 @@
 #' @format A dataframe with 62 rows and 8 variables
 #' \describe{
 #' \item {Number.of.kills}{approximate number of murders committed by each person}
-#' \item {Years}{The years of "operation"}
+#' \item {Years}{The years of operation}
 #' \item {Name}{Name of convicted serial killer}
 #' \item {AKA}{Some serial killers were given nicknames}
 #' \item {Year.Start}{Year the murders began}
 #' \item {Year.End}{Year the murders ended}
-#' \item {Date.of,first.kill}{Date, if known, first murder was committed}
+#' \item {Date.of.first.kill}{Date, if known, first murder was committed}
 #' \item {population..M.}{Estimated population of UK in millions at time of first murder}
 #' }
 #' @docType data
@@ -21,7 +21,7 @@ NULL
 #' @format A dataframe with 1019 rows and 9 variables
 #' \describe{
 #' \item{Latitude}{approximate latitude of murder}
-#' \item{Longitude} approximate longitude of murder}
+#' \item{Longitude}{approximate longitude of murder}
 #' \item{Sex}{biological sex of victim}
 #' \item{Age}{age of victim (years)}
 #' \item{Date}{month and day of murder}
@@ -36,10 +36,9 @@ NULL
 #' A dataset of retweets of NIWA's "viral" leopard seal tweet https://twitter.com/niwa_nz/status/1092610541401587712
 #' on the 5th Feb 2019
 #' @name retweetsNIWA
-#' @format A dataframe with 5317 rows and two variables
+#' @format A vector of length 4890
 #' \describe{
-#' \item{Date}{date of retweet}
-#' \item{Time}{time of retweet}
+#' \item{}{date and time of retweet}
 #' }
 NULL
 #' A dataset taken from the GeoNet Quake (http://quakesearch.geonet.org.nz/)
