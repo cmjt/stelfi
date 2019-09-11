a3783b5c7a5b0d73cd29c997b5ed5875aa5dd606
cmjt <cmjonestodd@gmail.com>
2019-09-11 17:41:45 +1200

    getting fitting function uts

--- R/fit_lgcp.r
+++ R/fit_lgcp.r
@@ -1,13 +1,13 @@
 #' creates a virtual class that is a superclass to the component classes so then both
 #' children inherit from that class
-setClassUnion("missing_or_spatialpolygon", c("missing", "SpatialPolygons")) 
+setClassUnion("missing_or_spatialpolygon", c("missing", "SpatialPolygonsDataFrame","SpatialPolygons")) 
 #' Function to fit a spatiotemporal log-Gaussian Cox process using TMB and the
 #' R_inla namespace for the spde construction of the latent field
 #' @param locs a matrix of locations 2xn
 #' @param temp.idx numeric aggregated temporal index 1,...,n 
 #' @param mesh a Delaney triangulation of the study area created using \code{INLA::inla.mesh.2d()}
-#' @param covs a 
-#' @param parameters a named vector of parameters. Must include "beta" the regression coefficients of fixed effects,
+#' @param covs a list of covariates at the mesh nodes for each time index
+#' @param parameters a named list of parameters. Must include "beta" the regression coefficients of fixed effects,
 #' "log_kappa" the smoothness parameter of the random field, if an AR(1) process for time is being fitted then
 #' "rho" must also be included as the temporal dependence parameter
 #' @param sp  optional spatial polygon of the domain. Should be supplied for lgcp as spde
@@ -17,15 +17,15 @@ setClassUnion("missing_or_spatialpolygon", c("missing", "SpatialPolygons"))
 #' \code{inla()}
 #' @export
 setGeneric("fit.lgcp",
-           function(locs,temp.idx, mesh, parameters, sp, tmb = TRUE,...){
+           function(locs,temp.idx, mesh, parameters, covs,sp, tmb = TRUE,...){
                standardGeneric("fit.lgcp")
            })
 
 setMethod("fit.lgcp",
-          c(locs = "matrix",temp.idx = "numeric",mesh = "inla.mesh",parameters = "vector",
-            sp = "missing_or_spatialpolygon" ,
+          c(locs = "matrix",temp.idx = "factor",mesh = "inla.mesh",parameters = "list",
+            covs = "list",sp = "missing_or_spatialpolygon" ,
             tmb = "logical"),
-          function(locs, temp.idx, mesh, parameters, sp, tmb, ...){
+          function(locs, temp.idx, mesh, parameters, covs, sp, tmb, ...){
               if(tmb == TRUE){
                   ## check for templates
                   if(!"lgcpar1"%in%getLoadedDLLs()){
@@ -42,13 +42,13 @@ setMethod("fit.lgcp",
                   data <- list(resp = resp, ID = as.factor(temp.idx),covariates = covs)
                   spde <- inla.spde2.matern(mesh = mesh,alpha = 2)
                   data$spde <- spde$param.inla[c("M0","M1","M2")]
-                  if(class(sp) == "SpatialPolygons"){
+                  if(class(sp) == "SpatialPolygonsDataFrame"){
                       w <- outwith(mesh = mesh, boundary = sp)
+                      data$area <- w*c(Matrix::diag(data$spde$M0))
                   }else{
-                      w <- rep(1,length(c(diag(data$spde$M0))))
+                      data$area <- c(Matrix::diag(data$spde$M0))
                   }
-                  data$area <- w*c(diag(data$spde$M0))
-                  ## PARAMS
+                  ## params
                   params <- list(beta = parameters[["beta"]],log_kappa = parameters[["log_kappa"]],
                                  x = matrix(0,nrow = mesh$n, ncol = 12),
                                  rho = parameters[["rho"]])
