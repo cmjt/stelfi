32fd5f1fca57b883d4fe51cc25e37e969d020d59
cmjt <cmjonestodd@gmail.com>
2020-07-31 14:10:35 +1200

    rewriting Cpp template for spatiotemporal lgcp

--- inst/src/lgcpar1.cpp
+++ inst/src/lgcpar1.cpp
@@ -1,14 +1,5 @@
-#include <TMB.hpp> // counts list (length one if model only spatial)
-template<class Type>
-struct counts_list : vector<vector <Type> >  {
-  counts_list(SEXP x){  /* x = List passed from R for counts at each time step */
-    (*this).resize(LENGTH(x));
-    for(int i=0; i<LENGTH(x); i++){
-      SEXP counts = VECTOR_ELT(x, i);
-      (*this)(i) = asVector<Type>(counts);
-    }
-  }
-};
+#include <TMB.hpp>
+
 template<class Type> // covariate list of matrices for each time step
 struct covariate_list : vector<matrix <Type> > {
   covariate_list(SEXP x){  /* x = List passed from R */
@@ -36,39 +27,56 @@ Type objective_function<Type>::operator() ()
   using namespace R_inla;
   using namespace density; // this where the structure for GMRF is defined
   using namespace Eigen;  // probably for sparseness calcs
-  DATA_STRUCT(resp, counts_list);
+  // Poisson response
+  DATA_MATRIX(y);
+  // covariates as a list of matricies
   DATA_STRUCT(covariates, covariate_list); // list of design matrcies for each time step
+  // weight of each mesh node
   DATA_VECTOR(area);
-  DATA_FACTOR(ID);
-  DATA_STRUCT(spde,spde_t); // this as structure of spde object is defined in r-inla hence using that namespce
-  PARAMETER_VECTOR(beta); // regression coefficients
-  PARAMETER(log_kappa); // kappa of random field
-  PARAMETER_ARRAY(x); //the random field/effect each matrix row is a time step
-  PARAMETER(rho); // parameter of the AR(1) temporal process (only applicable for spatio-temporal model)
-  PARAMETER(sigma); // scale parameter for the AR(1) temporal process (only applicable for spatio-temporal model)
-  int tsteps =  NLEVELS(ID); // number of time steps
-  Type kappa = exp(log_kappa); // return the kappa parameter of the Random field
+  // number of time steps
+  DATA_INTEGER(tsteps);
+  // this as structure of spde object is defined in r-inla hence using that namespce
+  DATA_STRUCT(spde,spde_t);
+  // vector of regression coefficients
+  PARAMETER_VECTOR(beta);
+  // Parameters for the AR(1) part of the spatiotemporal field
+  PARAMETER(log_rho);
+  PARAMETER(log_sigma);
+  // Parameter for the spatial GMRF of the spatiotemporal field
+  PARAMETER(log_kappa);
+  // array (n.y * tsteps) of spatiotemporal field random variables.
+  PARAMETER_ARRAY(field);
+  // transform, rho of AR1 process
+  Type rho = exp(log_rho);
+  // transform, sigma of AR1 process
+  Type sigma = exp(log_sigma);
+  // transform, kappa parameter of the field
+  Type kappa = exp(log_kappa);
+  // initialise neg log-likelihood
   Type nll = 0;
-  SparseMatrix<Type> Q = Q_spde(spde,kappa); // create the precision matrix from the spde model for the GMRF
-  nll = SEPARABLE(SCALE(AR1(rho),sigma), GMRF(Q))(x); // x the random effect is a GMRF with precision Q
+  // create the precision matrix from the spde model for the GMRF
+  SparseMatrix<Type> Q = Q_spde(spde,kappa);
+  // component due to spatiotemporal field with precision Q
+  nll = SEPARABLE(SCALE(AR1(rho),sigma), GMRF(Q))(field);
+  // data contribution
   for(int i = 0; i< (tsteps - 1); i++){
-    vector<Type> eta = covariates(i)*beta; // design matrix and regression coeffs. (fixed effects)
-    vector<Type> gmrf = (vector<Type> (x.col(i)));
-    
-    vector<Type> respi = resp(i);
+    // design matrix and regression coeffs. (fixed effects)
+    vector<Type> eta = covariates(i)*beta; 
+    vector<Type> gmrf = (vector<Type> (field.col(i)));
+    vector<Type> respi = y.col(i);
     for(int j = 0; j <respi.size(); j++){
       Type mu;
       mu = eta(j) +  gmrf(j);
       Type lambda = area(j)*exp(mu); // intensity
-      nll -= dpois_stable(respi(j),lambda,true);
+      nll -= dpois_stable(respi(j), lambda, true);
     }
    
   }
-  ADREPORT(kappa);
-  Type sigma2 = 1/(4*M_PI*pow(kappa,2));   // sigma2 = 1/(4*pi*k^2) with tau = 1
-  ADREPORT(sigma2);
-  Type range = sqrt(8)/kappa;   //Distance at which correlation has dropped to 0.1, see p. 4 in Lindgren et al. (2011)
-  ADREPORT(range);
+  // ADREPORT(kappa);
+  // Type sigma2 = 1/(4*M_PI*pow(kappa,2));   // sigma2 = 1/(4*pi*k^2) with tau = 1
+  // ADREPORT(sigma2);
+  // Type range = sqrt(8)/kappa;   //Distance at which correlation has dropped to 0.1, see p. 4 in Lindgren et al. (2011)
+  // ADREPORT(range);
   // SIMULATE {
   //   SEPARABLE(AR1(rho), GMRF(Q)).simulate(x);
   //   for(int i = 0; i< (tsteps - 1); i++){
